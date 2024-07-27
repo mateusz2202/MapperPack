11c9bff20c32736f287415d2f738311d3e007dcf
Mateusz Jóźwik <jmate2202@outlook.com>
2024-07-27 19:30:05 +0200

    Add project files.

---
+++ MapperPack/MapperDefinitions.cs
@@ -0,0 +1,21 @@
+﻿namespace MapperPack;
+
+public interface IMapper<TSource, TResult>
+{
+    Task<TResult> Map(TSource source);
+    Task<TSource> Map(TResult source);
+}
+
+
+public abstract class Mapper<TSource, TResult> : IMapper<TSource, TResult>
+{
+    public abstract Task<TResult> Map(TSource source);
+
+    public abstract Task<TSource> Map(TResult source);
+
+    public async Task<IEnumerable<TResult>> Map(IEnumerable<TSource> source)
+        => await Task.WhenAll(source.Select(Map));
+
+    public async Task<IEnumerable<TSource>> Map(IEnumerable<TResult> source)
+        => await Task.WhenAll(source.Select(Map));
+}
