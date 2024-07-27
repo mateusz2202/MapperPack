8979672e5bf16370e80ff1f258e27672854ba60e
Mateusz Jóźwik <jmate2202@outlook.com>
2024-07-27 20:12:27 +0200

    dodanie metod dla mappera

--- MapperPack/MapperDefinitions.cs
+++ MapperPack/MapperDefinitions.cs
@@ -1,25 +1,45 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MapperPack;
 
-public interface IMapper<TSource, TResult>
+public interface IMapper<TModel, TEntity>
 {
-    Task<TResult> Map(TSource source);
-    Task<TSource> Map(TResult source);
+    TModel Map(TEntity source);
+    TEntity Map(TModel source);
+    IEnumerable<TModel> Map(IEnumerable<TEntity> source);
+    IEnumerable<TEntity> Map(IEnumerable<TModel> source);
+
+    Task<TModel> MapAsync(TEntity source, CancellationToken cancellationToken = default);
+    Task<TEntity> MapAsync(TModel source, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TModel>> MapAsync(IEnumerable<TEntity> source, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> MapAsync(IEnumerable<TModel> source, CancellationToken cancellationToken = default);
 }
 
-
-public abstract class Mapper<TSource, TResult> : IMapper<TSource, TResult>
+public abstract class Mapper<TModel, TEntity> : IMapper<TModel, TEntity>
 {
-    public abstract Task<TResult> Map(TSource source);
+    public abstract TModel Map(TEntity source);
 
-    public abstract Task<TSource> Map(TResult source);
+    public abstract TEntity Map(TModel source);
 
-    public async Task<IEnumerable<TResult>> Map(IEnumerable<TSource> source)
-        => await Task.WhenAll(source.Select(Map));
 
-    public async Task<IEnumerable<TSource>> Map(IEnumerable<TResult> source)
-        => await Task.WhenAll(source.Select(Map));
+    public IEnumerable<TModel> Map(IEnumerable<TEntity> source)
+        => source.Select(Map);
+
+    public IEnumerable<TEntity> Map(IEnumerable<TModel> source)
+        => source.Select(Map);
+
+    public async Task<TModel> MapAsync(TEntity source, CancellationToken cancellationToken = default)
+        => await Task.FromResult(Map(source));
+
+    public async Task<TEntity> MapAsync(TModel source, CancellationToken cancellationToken = default)
+        => await Task.FromResult(Map(source));
+
+    public async Task<IEnumerable<TModel>> MapAsync(IEnumerable<TEntity> source, CancellationToken cancellationToken = default)
+        => await Task.WhenAll(source.Select(async entity => await MapAsync(entity, cancellationToken)));
+
+    public async Task<IEnumerable<TEntity>> MapAsync(IEnumerable<TModel> source, CancellationToken cancellationToken = default)
+        => await Task.WhenAll(source.Select(async entity => await MapAsync(entity, cancellationToken)));
 }
