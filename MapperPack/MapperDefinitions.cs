88b411dbdf065f77ac9dcf211cdfdeb51da4e8f6
Mateusz Jóźwik <jmate2202@outlook.com>
2024-07-27 19:32:56 +0200

    dodanie kodu

--- MapperPack/MapperDefinitions.cs
+++ MapperPack/MapperDefinitions.cs
@@ -1,4 +1,8 @@
-﻿namespace MapperPack;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapperPack;
 
 public interface IMapper<TSource, TResult>
 {
